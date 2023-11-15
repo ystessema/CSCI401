4ff361a4de1bde3b595f3d49326d2d879c9dbe79
ystessema <104567949+ystessema@users.noreply.github.com>
2023-11-15 17:47:03 -0500

    Update pipes_processes1.c

--- Lab-3-part-1/pipes_processes1.c
+++ Lab-3-part-1/pipes_processes1.c
@@ -15,8 +15,10 @@ int main()
     int fd1[2];  // Used to store two ends of first pipe 
     int fd2[2];  // Used to store two ends of second pipe 
   
-    char fixed_str[] = "howard.edu"; 
+    char fixed_str1[] = "howard.edu";
+    char fixed_str2[] = "gobison.org";
     char input_str[100]; 
+    char concat_str[200];
     pid_t p; 
   
     if (pipe(fd1)==-1) 
@@ -45,44 +47,42 @@ int main()
     { 
   
         close(fd1[0]);  // Close reading end of pipes 
-        close(fd2[0]);
+        close(fd2[1]);
   
         // Write input string and close writing end of first 
         // pipe. 
         write(fd1[1], input_str, strlen(input_str)+1); 
-        
+        close(fd1[1]);
   
         // Wait for child to send a string 
         wait(NULL); 
-  
-        close(fd2[1]); // Close writing end of pipes 
-        close(fd1[1]); 
+
+        read(fd2[0], concat_str, 100);
+        strcat(concat_str, fixed_str2);
+        printf("Concatenated string: %s\n", concat_str);
+        
+        close(fd2[0]); // Close writing end of pipes 
+         
     } 
   
     // child process 
     else
     { 
         close(fd1[1]);  // Close writing end of first pipes 
-        close(fd2[1]); 
-      
-        // Read a string using first pipe 
-        char concat_str[100]; 
-        read(fd1[0], concat_str, 100); 
-  
-        // Concatenate a fixed string with it 
-        int k = strlen(concat_str); 
-        int i; 
-        for (i=0; i<strlen(fixed_str); i++) 
-            concat_str[k++] = fixed_str[i]; 
-  
-        concat_str[k] = '\0';   // string ends with '\0' 
-  
-        printf("Concatenated string %s\n", concat_str);
-        // Close both reading ends 
-        close(fd1[0]); 
         close(fd2[0]); 
+      
+        read(fd1[0], concat_str, 100);
+        strcat(concat_str, fixed_str1);
+        printf("Concatenated string: %s\n", concat_str);
+        
 
-  
+        printf("Enter another string to send back: ");
+        scanf("%s", input_str);
+
+        write(fd2[1], input_str, strlen(input_str)+1);
+        close(fd1[0]); 
+        close(fd2[1]);
+        
         exit(0); 
     } 
 } 
