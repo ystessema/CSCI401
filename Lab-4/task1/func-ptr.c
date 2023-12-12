5dbabc56989b9a81cb23b25183bfe833ede3c4f4
ystessema <104567949+ystessema@users.noreply.github.com>
2023-12-12 00:54:41 -0500

    Update func-ptr.c

--- Lab-4/task1/func-ptr.c
+++ Lab-4/task1/func-ptr.c
@@ -6,81 +6,67 @@
 #include "process.h"
 #include "util.h"
 
-#define DEBUG 0			//change this to 1 to enable verbose output
+#define DEBUG 0         // Change this to 1 to enable verbose output
 
-/**
- * Signature for an function pointer that can compare
- * You need to cast the input into its actual 
- * type and then compare them according to your
- * custom logic
- */
 typedef int (*Comparer) (const void *a, const void *b);
 
-/**
- * compares 2 processes
- * You can assume: 
- * - Process ids will be unique
- * - No 2 processes will have same arrival time
- */
-int my_comparer(const void *this, const void *that)
-{
-	//TODO: IMPLEMENT ME!
-	return 0;
+int my_comparer(const void *this, const void *that) {
+    // Cast the void pointers to Process pointers
+    const Process *process1 = (const Process *)this;
+    const Process *process2 = (const Process *)that;
+
+    // First, compare based on priority
+    if (process1->priority > process2->priority) {
+        return -1;
+    } else if (process1->priority < process2->priority) {
+        return 1;
+    } else {
+        // If the priority is the same, then sort based on arrival time
+        if (process1->arrival_time < process2->arrival_time) {
+            return -1;
+        } else if (process1->arrival_time > process2->arrival_time) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-	if (argc < 2) {
-		   fprintf(stderr, "Usage: ./func-ptr <input-file-path>\n");
-		   fflush(stdout);
-		   return 1;
-	}
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./func-ptr <input-file-path>\n");
+        fflush(stdout);
+        return 1;
+    }
 
-	/*******************/
-	/* Parse the input */
-	/*******************/
-	FILE *input_file = fopen(argv[1], "r");
-	if (!input_file) {
-		   fprintf(stderr, "Error: Invalid filepath\n");
-		   fflush(stdout);
-		   return 1;
-	}
+    FILE *input_file = fopen(argv[1], "r");
+    if (!input_file) {
+        fprintf(stderr, "Error: Invalid filepath\n");
+        fflush(stdout);
+        return 1;
+    }
 
-	Process *processes = parse_file(input_file);
+    Process *processes = parse_file(input_file);
 
-	/*******************/
-	/* sort the input  */
-	/*******************/
-	Comparer process_comparer = &my_comparer;
+    Comparer process_comparer = &my_comparer;
 
 #if DEBUG
-	for (int i = 0; i < P_SIZE; i++) {
-		    printf("%d (%d, %d) ",
-				processes[i].pid,
-				processes[i].priority, processes[i].arrival_time);
-	}
-	printf("\n");
+    for (int i = 0; i < P_SIZE; i++) {
+        printf("%d (%d, %d) ", processes[i].pid, processes[i].priority, processes[i].arrival_time);
+    }
+    printf("\n");
 #endif
 
-	qsort(processes, P_SIZE, sizeof(Process), process_comparer);
+    qsort(processes, P_SIZE, sizeof(Process), process_comparer);
 
-	/**************************/
-	/* print the sorted data  */
-	/**************************/
+    for (int i = 0; i < P_SIZE; i++) {
+        printf("%d (%d, %d)\n", processes[i].pid, processes[i].priority, processes[i].arrival_time);
+    }
+    fflush(stdout);
+    fflush(stderr);
 
-	for (int i = 0; i < P_SIZE; i++) {
-		    printf("%d (%d, %d)\n",
-				processes[i].pid,
-				processes[i].priority, processes[i].arrival_time);
-	}
-	fflush(stdout);
-	fflush(stderr);
-
-	/************/
-	/* clean up */
-	/************/
-	free(processes);
-	fclose(input_file);
-	return 0;
+    free(processes);
+    fclose(input_file);
+    return 0;
 }
